14f2c6b3d67ff12a8b0ce83ecb69ce5f773781fb
Luka Micheletti <michelettiluka@gmail.com>
2020-12-06 15:03:31 +0100

    Started rope automation code

---
+++ Assets/Scripts/Rughai/Prop/Rope/RopeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour {
+    private List<GameObject> children;
+
+    void Start() {
+        // Retrieve all children objects.
+        foreach (Transform child in transform) {
+            this.children.Add(child.gameObject);
+        }
+    }
+}
