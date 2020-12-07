c96ae74c6924575e55983a6a288b2bac18f605b0
Luka Micheletti <michelettiluka@gmail.com>
2020-12-08 00:15:10 +0100

    Rope controller more or less working + Duk

--- Assets/Scripts/Rughai/Prop/Rope/RopeController.cs
+++ Assets/Scripts/Rughai/Prop/Rope/RopeController.cs
@@ -23,11 +23,9 @@ public class RopeController : MonoBehaviour {
         for (int i = 0; i < this.anchors.Count - 1; i++) {
             // Compute distance.
             this.distances.Add(this.anchors[i + 1].transform.position - this.anchors[i].transform.position);
-            Debug.Log("Distance " + this.distances[i]);
 
             // Compute angle.
             this.angles.Add(Utils.AngleBetween(this.anchors[i].transform.position, this.anchors[i + 1].transform.position));
-            Debug.Log("Angle " + this.angles[i]);
         }
 
         // Loop through anchors in order to create segments in between.
@@ -38,21 +36,28 @@ public class RopeController : MonoBehaviour {
             // Create segments between anchors.
             List<GameObject> ropeSegments = new List<GameObject>();
             for (int j = 0; j < amount; j++) {
-                GameObject ropeSegment = Instantiate(this.ropeSegmentType, new Vector2(this.anchors[i].transform.position.x + (j + 1) / 2, this.anchors[i].transform.position.y), Quaternion.identity);
+                GameObject ropeSegment = Instantiate(this.ropeSegmentType, new Vector2(this.anchors[i].transform.position.x + (j + 1) / 2f, this.anchors[i].transform.position.y), Quaternion.identity);
                 ropeSegment.transform.parent = this.transform;
                 ropeSegments.Add(ropeSegment);
             }
 
             // Add hinge component to the first anchor and attach it to the first segment.
-            (this.anchors[i].AddComponent<HingeJoint2D>() as HingeJoint2D).connectedBody = ropeSegments[0].GetComponent<Rigidbody2D>();
+            HingeJoint2D firstJoint = this.anchors[i].AddComponent<HingeJoint2D>() as HingeJoint2D;
+            firstJoint.connectedBody = ropeSegments[0].GetComponent<Rigidbody2D>();
+            firstJoint.anchor = new Vector2(0.25f, 0.511f);
 
             // Loop through rope segments in order to attach hinges to them.
             for (int j = 0; j < amount - 1; j++) {
-                (ropeSegments[j].AddComponent<HingeJoint2D>() as HingeJoint2D).connectedBody = ropeSegments[j + 1].GetComponent<Rigidbody2D>();;
+                HingeJoint2D segmentJoint = ropeSegments[j].AddComponent<HingeJoint2D>() as HingeJoint2D;
+                segmentJoint.connectedBody = ropeSegments[j + 1].GetComponent<Rigidbody2D>();
+                segmentJoint.anchor = new Vector2(0.25f, 0.511f);
             }
 
             // Add hinge component to the last rope segment and attach it to the next anchor.
-            (ropeSegments[amount - 1].AddComponent<HingeJoint2D>() as HingeJoint2D).connectedBody = this.anchors[i + 1].GetComponent<Rigidbody2D>();
+            // (ropeSegments[amount - 1].AddComponent<HingeJoint2D>() as HingeJoint2D).connectedBody = this.anchors[i + 1].GetComponent<Rigidbody2D>();
+            HingeJoint2D lastJoint = ropeSegments[amount - 1].AddComponent<HingeJoint2D>() as HingeJoint2D;
+            lastJoint.connectedBody = this.anchors[i + 1].GetComponent<Rigidbody2D>();
+            lastJoint.anchor = new Vector2(0.25f, 0.511f);
         }
     }
 }
