2e59cb48f61dd1170753740eb3f34a41b53e091c
Luka Micheletti <michelettiluka@gmail.com>
2020-12-27 15:05:06 +0100

    Started parallax background

---
+++ Assets/Scripts/Background/Parallax2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax2D : MonoBehaviour {
+    public Transform target;
+    public Vector2 offset;
+
+    void Start() {
+        this.transform.position = this.target.position;
+    }
+
+    void FixedUpdate() {
+        this.transform.position = (Vector2) this.target.position + this.offset;
+
+        Transform anchor = this.target.GetComponent<CameraTargetController>().anchor;
+        Vector2 offset = (Vector2) anchor.position - (Vector2) this.target.position;
+
+        for (int i = 0; i < this.transform.childCount; i++) {
+            Transform childTransform = this.transform.Find(i.ToString());
+            childTransform.position = ((Vector2) this.transform.position) + (offset * (i * 0.1f + 1));
+        }
+    }
+}
--- Assets/Scripts/Camera/CameraTargetController.cs
+++ Assets/Scripts/Camera/CameraTargetController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CameraTargetController : MonoBehaviour {
     public float extension;
-    public Transform pole;
+    public Transform anchor;
     public CameraInput input;
     public Vector2 offset;
 
     void Awake() {
-        this.transform.position = (Vector2) this.pole.position + this.offset;
+        this.transform.position = (Vector2) this.anchor.position + this.offset;
     }
 
     void FixedUpdate() {
-        this.transform.position = (Vector2) this.pole.transform.position + Utils.PolarToCartesian(this.input.moveDir, this.input.moveLen * this.extension) +  this.offset;
+        this.transform.position = (Vector2) this.anchor.transform.position + Utils.PolarToCartesian(this.input.moveDir, this.input.moveLen * this.extension) +  this.offset;
     }
 }
