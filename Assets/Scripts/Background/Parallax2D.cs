ab1562bd00b49db70c1ecf6c416a2f1272cb95d4
Luka Micheletti <michelettiluka@gmail.com>
2020-12-29 00:49:24 +0100

    Fixed camera-bound parallax

--- Assets/Scripts/Background/Parallax2D.cs
+++ Assets/Scripts/Background/Parallax2D.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Parallax2D : MonoBehaviour {
     public Transform target;
-    public Vector2 offset;
     public float speed;
 
     void Start() {
@@ -12,17 +11,30 @@ public class Parallax2D : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        this.transform.position = (Vector2) this.target.position + this.offset;
+        float dir = Utils.AngleBetween(this.transform.position, this.target.position);
+        float len = Vector2.Distance(this.transform.position, this.target.position) * this.speed;
+        this.transform.position = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len);
 
         Transform anchor = this.target.GetComponent<CameraTargetController>().anchor;
         Vector2 offset = (Vector2) anchor.position - (Vector2) this.target.position;
 
         for (int i = 0; i < this.transform.childCount; i++) {
             Transform childTransform = this.transform.Find(i.ToString());
-            Vector2 childPosition = new Vector2();
-            childPosition.x = this.transform.position.x;
-            childPosition.y = this.transform.position.y + (offset.y * ((i + 1) * 0.1f));
-            childTransform.position = childPosition;
+            // Vector2 targetPosition = new Vector2();
+            // targetPosition.x = this.transform.position.x;
+            // targetPosition.y = (this.transform.position.y) + (offset.y * ((this.transform.childCount - i) * 0.1f));
+            // childTransform.position = targetPosition;
+
+            // Vector2 childPosition = new Vector2();
+            // childPosition.x = targetPosition.x;
+            // childPosition.y = this.transform.position.y + targetPosition.y;
+
+
+
+            dir = Utils.AngleBetween(Vector2.zero, offset);
+            len = (Vector2.Distance(Vector2.zero, offset) + (offset.magnitude * (this.transform.childCount - i))) * this.speed * 0.5f;
+
+            childTransform.position = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len);
         }
     }
 }
