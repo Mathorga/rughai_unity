59e054abfe8ea1ca22b7ed63bbdb0f7d0c7535bf
Luka Micheletti <michelettiluka@gmail.com>
2021-01-11 19:37:21 +0100

    Enlarged Rughai BG

--- Assets/Scripts/Background/Parallax2D.cs
+++ Assets/Scripts/Background/Parallax2D.cs
@@ -45,7 +45,7 @@ public class Parallax2D : MonoBehaviour {
             // Get child by its name.
             Transform childTransform = this.transform.Find(i.ToString());
 
-            childTransform.position = this.target.position * ((i * 0.05f) + 1);
+            childTransform.position = this.target.position * ((i * this.speed) + 1);
         }
     }
 }
