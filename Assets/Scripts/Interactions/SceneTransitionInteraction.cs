fb04a88d9664a90f6eb3a69576c707c80b3a76ac
Luka Micheletti <michelettiluka@gmail.com>
2021-01-24 00:59:30 +0100

    Implemented scene transition interaction

--- Assets/Scripts/Interactions/SceneTransitionInteraction.cs
+++ Assets/Scripts/Interactions/SceneTransitionInteraction.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransitionInteraction : Interaction {
+    public string nextScene;
+    public Animator sceneAnimator;
+    public Vector2 nextPlayerPosition;
+    public Vector2Value playerPosition;
+
     void FixedUpdate() {
         if (this.run) {
             // Interact.
             Debug.Log("Anselmoide Anemocomoro " + this.gameObject.ToString());
 
+            //TODO Disable input.
+
+            this.playerPosition.value = this.nextPlayerPosition;
+            this.StartCoroutine(this.LoadScene());
+
             // Reset trigger after interaction is complete.
             this.run = false;
         }
+    }
+
+    IEnumerator LoadScene() {
+        this.sceneAnimator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(this.nextScene);
     }
 }
