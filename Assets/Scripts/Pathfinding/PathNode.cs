e3144923b12d93b6092eee4e47f25c35c55b20b0
Luka Micheletti <michelettiluka@gmail.com>
2021-02-18 23:50:30 +0100

    Started pathfinding logic

--- Assets/Scripts/Pathfinding/PathNode.cs
+++ Assets/Scripts/Pathfinding/PathNode.cs
@@ -3,19 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PathNode {
+    private const int STRAIGHT_COST = 10;
+    private const int DIAGONAL_COST = 14;
+
     private Field<PathNode> field;
-    private int x;
-    private int y;
+    public int x {
+        get;
+        private set;
+    }
+    public int y {
+        get;
+        private set;
+    }
 
-    private int gCost;
-    private int hCost;
-    private int fCost;
+    public PathNode previous {
+        get;
+        set;
+    }
 
-    private PathNode previous;
+    public int gCost {
+        get;
+        set;
+    }
+    public int hCost {
+        get;
+        private set;
+    }
+    public int fCost {
+        get;
+        private set;
+    }
 
     public PathNode(Field<PathNode> field, int x, int y) {
         this.field = field;
         this.x = x;
         this.y = y;
+    }
+
+    public List<PathNode> GetNeighbors() {
+        //TODO
+        return null;
+    }
+
+    public void ComputeHCost(PathNode goal) {
+        int xDistance = Mathf.Abs(this.x - goal.x);
+        int yDistance = Mathf.Abs(this.y - goal.y);
+        int remaining = Mathf.Abs(xDistance - yDistance);
+        this.hCost = STRAIGHT_COST * Mathf.Min(xDistance, yDistance) + DIAGONAL_COST * remaining;
+    }
+
+    public void ComputeFCost() {
+        this.fCost = this.gCost + this.hCost;
+    }
+
+    public override string ToString() {
+        return this.x + " | " + this.y;
     }
 }
--- Assets/Scripts/Pathfinding/Pathfinding.cs
+++ Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -8,6 +8,9 @@ public class Pathfinding {
         private set;
     }
 
+    private List<PathNode> openList;
+    private List<PathNode> closedList;
+
     public Pathfinding(int width, int height) {
         this.field = new Field<PathNode>(width,
                                          height,
@@ -19,4 +22,59 @@ public class Pathfinding {
 
     }
 
+    private List<PathNode> ComputePath(int startX, int startY, int endX, int endY) {
+        // Instantiate lists.
+        this.openList = new List<PathNode>();
+        this.closedList = new List<PathNode>();
+
+        // Retrieve element at start position.
+        PathNode startNode = this.field.GetElement(startX, startY);
+        PathNode endNode = this.field.GetElement(endX, endY);
+        this.openList.Add(startNode);
+
+        // Initialise the field.
+        for (int i = 0; i < this.field.width; i++) {
+            for (int j = 0; j < this.field.height; j++) {
+                PathNode pathNode = this.field.GetElement(i, j);
+                pathNode.gCost = int.MaxValue;
+                pathNode.ComputeFCost();
+                pathNode.previous = null;
+            }
+        }
+
+        startNode.gCost = 0;
+        startNode.ComputeHCost(endNode);
+        startNode.ComputeFCost();
+
+        while (this.openList.Count > 0) {
+            PathNode currentNode = this.GetFastestNode(this.openList);
+
+            if (currentNode == endNode) {
+                // Found the solution, so trace back and return the complete path.
+                return RetrievePath(endNode);
+            } else {
+                this.openList.Remove(currentNode);
+                this.closedList.Add(currentNode);
+            }
+        }
+
+        return this.openList;
+    }
+
+    private List<PathNode> RetrievePath(PathNode end) {
+        //TODO
+        return null;
+    }
+
+    private PathNode GetFastestNode(List<PathNode> nodes) {
+        PathNode result = nodes[0];
+
+        for (int i = 0; i < nodes.Count; i++) {
+            if (nodes[i].fCost < result.fCost) {
+                result = nodes[i];
+            }
+        }
+
+        return result;
+    }
 }
