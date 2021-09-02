3d9eaab5d56ad2d2ab9d6a30e9db2d952d71daa2
Luka Micheletti <michelettiluka@gmail.com>
2021-09-03 00:30:35 +0200

    Added shader for merging transparencies and fixed parallax BG

--- Assets/Scripts/Background/Parallax2D.cs
+++ Assets/Scripts/Background/Parallax2D.cs
@@ -1,54 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Parallax2D : MonoBehaviour {
     public Transform target;
-    public Vector2 offset;
     public float speed;
-    public bool absolute;
-
-    void Start() {
-        this.transform.position = (Vector2) this.target.position;
-    }
 
     void FixedUpdate() {
-        if (this.absolute) {
-            this.MoveAbsolute();
-        } else {
-            this.MoveRelative();
-        }
+        this.Move();
     }
 
-    void MoveRelative() {
-        Transform anchor = this.target.GetComponent<CameraTargetController>().anchor;
-
-        float dir = Utils.AngleBetween(this.transform.position, anchor.position);
-        float len = Vector2.Distance(this.transform.position, anchor.position) * this.speed;
-        this.transform.position = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len) + this.offset;
-
-        Vector2 anchorOffset = (Vector2) anchor.position - (Vector2) this.target.position;
-
+    void Move() {
         for (int i = 0; i < this.transform.childCount; i++) {
             // Get child by its name.
             Transform childTransform = this.transform.Find(i.ToString());
 
-            dir = Utils.AngleBetween(Vector2.zero, anchorOffset);
-            len = (Vector2.Distance(Vector2.zero, anchorOffset) + (anchorOffset.magnitude * (this.transform.childCount - i * 2f))) * this.speed * 0.5f;
-
-            childTransform.position = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len);
-        }
-    }
-
-    void MoveAbsolute() {
-        for (int i = 0; i < this.transform.childCount; i++) {
-            // Get child by its name.
-            Transform childTransform = this.transform.Find(i.ToString());
-
-            // float xPos = this.target.position.x * (((-i) * this.speed) + 1);
-            // float yPos = this.target.position.y * ((i * this.speed) + 1);
-            // childTransform.position = new Vector2(xPos, yPos);
-            childTransform.position = this.target.position * (((-i) * this.speed) + 1);
+            childTransform.position = this.transform.position + (this.target.position * (((-i) * this.speed) + 1));
         }
     }
 }
