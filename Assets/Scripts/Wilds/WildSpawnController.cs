28672298f6148409e2bdb4103c39f6baaa3619e4
Luka Micheletti <michelettiluka@gmail.com>
2022-04-17 22:03:51 +0200

    Added spawners

--- Assets/Scripts/Wilds/WildSpawnController.cs
+++ Assets/Scripts/Wilds/WildSpawnController.cs
@@ -9,19 +9,33 @@ public class WildSpawnController : MonoBehaviour {
     // The amount of wild to spawn.
     public int amount;
 
-    // Radius in which the spawn extends.
-    public float radius;
+    // Width over which to extend the spawn.
+    public float width;
+
+    // Height over which to extend the spawn.
+    public float height;
 
     void Start() {
         // Dummily instantiate amount Gameobjects
         for (int i = 0; i < this.amount; i++) {
+            // Randomly place the current wild inside the given area.
+            float wildPosX = this.transform.position.x + Random.Range(-(this.width / 2), this.width / 2);
+            float wildPosY = this.transform.position.y + Random.Range(-(this.height / 2), this.height / 2);
+
+            // Create the wild in the computed position.
             GameObject ropeSegment = Instantiate(this.wild,
-                                                 new Vector2(this.transform.position.x + Random.Range(-this.radius, this.radius), this.transform.position.y + Random.Range(-this.radius, this.radius)),
+                                                 new Vector2(wildPosX, wildPosY),
                                                  Quaternion.identity);
         }
     }
 
     void Update() {
-        
+        // TODO.
+    }
+
+    void OnDrawGizmos() {
+        // Draw a solid transparent rectangle (0 thickness) to represent the spawn area.
+        Gizmos.color = new Color(1.0f, 1.0f, 0.0f, 0.5f);
+        Gizmos.DrawCube(this.transform.position, new Vector3(this.width, this.height, 0.0f));
     }
 }
