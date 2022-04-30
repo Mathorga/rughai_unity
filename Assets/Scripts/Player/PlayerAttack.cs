ae2ff67ce68584476f6d519f7f4009dcfc37b5af
Luka Micheletti <michelettiluka@gmail.com>
2022-04-30 23:14:49 +0200

    Added layer filtering for attacks

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -6,6 +6,7 @@ public class PlayerAttack : MonoBehaviour {
     private PlayerController controller;
     private AudioSource audioSource;
 
+    public LayerMask hitLayer;
     public AudioClip swingSound;
 
     public float extent;
@@ -29,6 +30,33 @@ public class PlayerAttack : MonoBehaviour {
     }
 
     void FixedUpdate() {
+        this.PlaySound();
+
+        if ((this.controller.state == PlayerController.State.Atk0 ||
+            this.controller.state == PlayerController.State.Atk1 ||
+            this.controller.state == PlayerController.State.Atk2) &&
+            this.controller.animationProgress > 0.4f &&
+            this.controller.animationProgress < 0.6f) {
+            this.Attack();
+        }
+    }
+
+    void OnDrawGizmos() {
+        if (Application.isPlaying) {
+            // Calculate the application point of the attack.
+            float dir = this.controller.faceDir;
+            float len = this.extent;
+
+            Vector2 pos2D = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len);
+            pos2D[1] += this.elevation;
+
+            Gizmos.color = new Color(1.0f, 1.0f, 0.0f, 0.5f);
+            Gizmos.DrawWireSphere(pos2D, len);
+        }
+    }
+
+    // Plays the attack sound according to state.
+    void PlaySound() {
         if (this.controller.state == PlayerController.State.Atk0 ||
             this.controller.state == PlayerController.State.Atk1) {
             if (this.controller.state != this.currentState) {
@@ -54,52 +82,33 @@ public class PlayerAttack : MonoBehaviour {
                 this.currentState = this.controller.state;
             }
         }
+    }
 
-        if ((this.controller.state == PlayerController.State.Atk0 ||
-            this.controller.state == PlayerController.State.Atk1 ||
-            this.controller.state == PlayerController.State.Atk2) &&
-            this.controller.animationProgress > 0.4f &&
-            this.controller.animationProgress < 0.6f) {
+    // Performs the attack.
+    void Attack() {
+        // Calculate the application point of the attack.
+        float dir = this.controller.faceDir;
+        float len = this.extent;
 
-            // Calculate the application point of the attack.
-            float dir = this.controller.faceDir;
-            float len = this.extent;
+        // Compute attack position with elevation.
+        Vector2 pos2D = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len);
+        pos2D[1] += this.elevation;
 
-            // Compute attack position with elevation.
-            Vector2 pos2D = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len);
-            pos2D[1] += this.elevation;
+        // Check what was hit by the attack.
+        Collider2D[] hits = Physics2D.OverlapCircleAll(pos2D, this.extent, this.hitLayer);
 
-            // Check what was hit by the attack.
-            Collider2D[] hits = Physics2D.OverlapCircleAll(pos2D, this.extent);
+        // Only perform screen shake if a camera shake behavior is provided.
+        if (this.cameraShake != null) {
+            this.cameraShake.Shake(this.shakeDuration * hits.Length, this.shakeMagnitude * hits.Length, this.shakeFade * hits.Length);
+        }
 
-            // Only perform screen shake if a camera shake behavior is provided.
-            if (this.cameraShake != null) {
-                this.cameraShake.Shake(this.shakeDuration * hits.Length, this.shakeMagnitude * hits.Length, this.shakeFade * hits.Length);
-            }
-
-            // Loop through hits.
-            foreach (Collider2D hit in hits) {
-                // TODO Damage.
-                HitController hitController = hit.GetComponent<HitController>();
-                if (hitController != null) {
-                    hitController.takeDamage(this.transform, 1);
-                }
+        // Loop through hits.
+        foreach (Collider2D hit in hits) {
+            // TODO Damage.
+            HitController hitController = hit.GetComponent<HitController>();
+            if (hitController != null) {
+                hitController.takeDamage(this.transform, 1);
             }
         }
     }
-
-    void OnDrawGizmos() {
-        if (Application.isPlaying) {
-            // Calculate the application point of the attack.
-            float dir = this.controller.faceDir;
-            float len = this.extent;
-
-            Vector2 pos2D = (Vector2) this.transform.position + Utils.PolarToCartesian(dir, len);
-            pos2D[1] += this.elevation;
-
-            Gizmos.color = new Color(1.0f, 1.0f, 0.0f, 0.5f);
-            Gizmos.DrawWireSphere(pos2D, len);
-        }
-    }
-
 }
